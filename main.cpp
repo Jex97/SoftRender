732ce32a85ec7c9aadb2f1987034e63cd60aa76c
XJ-LapTop <746806139@qq.com>
2023-02-28 16:13:36 +0800

    add zBuffer

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
-const int IMAGE_HEIGHT = 200;
-const int IMAGE_WIDTH = 200;
+const int IMAGE_HEIGHT = 800;
+const int IMAGE_WIDTH = 800;
 
 const Vec3f LIGHT = Vec3f(0, 0, -1);
 Model *model = nullptr;
@@ -47,13 +47,14 @@ void DrawLine(int x0, int y0, int x1, int y1, TGAImage& image, const TGAColor& c
 void DrawLine(const Vec2i& v0, const Vec2i& v1, TGAImage& image, const TGAColor& color){
     DrawLine(v0.x, v0.y, v1.x, v1.y, image, color);
 }
-Vec3f CalcBaryCentric(Vec2i* pos, const Vec2i& P){
+Vec3f CalcBaryCentric(Vec3f* pos, const Vec3f& P){
     Vec3f res = Vec3f(pos[2].x - pos[0].x, pos[1].x - pos[0].x, pos[0].x - P.x)
             ^ Vec3f (pos[2].y - pos[0].y, pos[1].y - pos[0].y, pos[0].y - P.y);
-    if(std::abs(res.z) < 1) return Vec3f (-1, 1, 1);
+    if(std::abs(res.z) < 1e-2) return Vec3f (-1, 1, 1);
     return Vec3f(1.f-(res.x+res.y)/res.z, res.y/res.z, res.x/res.z);
 }
-void DrawTriangle_Sweep(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+
+void DrawTriangle_Sweep(Vec3f t0, Vec3f t1, Vec3f t2, TGAImage &image, TGAColor color) {
     if(t0.y == t1.y && t0.y == t2.y) return ;    // important!
     // sort the vertices, t0, t1, t2 lower?to?upper
     if (t0.y>t1.y) std::swap(t0, t1);
@@ -65,8 +66,8 @@ void DrawTriangle_Sweep(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor 
         int segment_height = t1.y-t0.y+1;
         float alpha = (float)(y-t0.y)/total_height;
         float beta  = (float)(y-t0.y)/segment_height; // be careful with divisions by zero
-        Vec2i A = t0 + (t2-t0)*alpha;
-        Vec2i B = t0 + (t1-t0)*beta;
+        Vec3f A = t0 + (t2-t0)*alpha;
+        Vec3f B = t0 + (t1-t0)*beta;
 
         if (A.x>B.x) std::swap(A, B);
         for (int j=A.x; j<=B.x; j++) {
@@ -78,40 +79,50 @@ void DrawTriangle_Sweep(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor 
         int segment_height =  t2.y-t1.y+1;
         float alpha = (float)(y-t0.y)/total_height;
         float beta  = (float)(y-t1.y)/segment_height; // be careful with divisions by zero
-        Vec2i A = t0 + (t2-t0)*alpha;
-        Vec2i B = t1 + (t2-t1)*beta;
+        Vec3f A = t0 + (t2-t0)*alpha;
+        Vec3f B = t1 + (t2-t1)*beta;
         if (A.x>B.x) std::swap(A, B);
         for (int j=A.x; j<=B.x; j++) {
             image.set(j, y, color); // attention, due to int casts t0.y+i != A.y
         }
     }
 }
-void DrawTriangle_Barycentric(Vec2i * pos, TGAImage& image, const TGAColor& color){
-    Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
-    Vec2i bboxmax(0, 0);
+
+void DrawTriangle(Vec3f* pos, TGAImage& image, const TGAColor& color){
+    DrawTriangle_Sweep(pos[0], pos[1], pos[2], image, color);
+    //DrawTriangle_Barycentric(pos, image, color);
+}
+
+
+void DrawTriangle_zBuffer(Vec3f* pos, float* zBuffer, TGAImage& image, const TGAColor& color){
+    Vec2f bboxmin(image.get_width() - 1, image.get_height() - 1);
+    Vec2f bboxmax(0, 0);
     for(int i = 0; i < 3; ++i){
         bboxmax.x = std::max(bboxmax.x, pos[i].x);
         bboxmax.y = std::max(bboxmax.y, pos[i].y);
         bboxmin.x = std::min(bboxmin.x, pos[i].x);
         bboxmin.y = std::min(bboxmin.y, pos[i].y);
     }
-    Vec2i P;
+    Vec3f P;
     for(P.x = bboxmin.x; P.x <= bboxmax.x; ++P.x){
         for(P.y = bboxmin.y; P.y <= bboxmax.y; ++P.y){
             Vec3f u = CalcBaryCentric(pos, P);
             if(u.x < 0 || u.y < 0 || u.z < 0) continue;
-            image.set(P.x, P.y, color);
+
+            P.z = 0;
+            P.z = pos[0].z * u.x + pos[1].z * u.y + pos[2].z * u.z;
+
+            if(zBuffer[int(P.x + P.y*800)] < P.z){
+                zBuffer[int(P.x + P.y*800)] = P.z;
+                image.set(P.x, P.y, color);
+            }
         }
     }
 }
-void DrawTriangle(Vec2i* pos, TGAImage& image, const TGAColor& color){
-    //DrawTriangle_Sweep(pos[0], pos[1], pos[2], image, color);
-    DrawTriangle_Barycentric(pos, image, color);
-}
-void GetScreenPos(const Vec2f& inputP0, Vec2i& outputP0){
-    outputP0.x = (inputP0.x+1.)*IMAGE_WIDTH/2.;
-    outputP0.y = (inputP0.y+1.)*IMAGE_HEIGHT/2.;
-
+void GetScreenPos(const Vec3f& inputP0, Vec3f& outputP0){    // ensure x, y are ints.
+    outputP0.x = int((inputP0.x+1.)*IMAGE_WIDTH/2. + 0.5);
+    outputP0.y = int((inputP0.y+1.)*IMAGE_HEIGHT/2. + 0.5);
+    outputP0.z = inputP0.z;
 }
 int main(int argc, char** argv) {
 
@@ -127,22 +138,24 @@ int main(int argc, char** argv) {
     model_name = model_name.substr(0, model_name.find('.'));
 
     TGAImage image(IMAGE_WIDTH, IMAGE_HEIGHT, TGAImage::RGB);
+    float *zBuffer = new float[IMAGE_WIDTH * IMAGE_HEIGHT];
+    for(int i = 0; i < IMAGE_HEIGHT * IMAGE_WIDTH; ++i) zBuffer[i] = -std::numeric_limits<float>::max();
     for (int i=0; i<model->nfaces(); i++) {
         Vec3f face_pos[3];
-        Vec2i screen_pos[3];
+        Vec3f screen_pos[3];
         for (int j=0; j<3; j++) {
             face_pos[j].x = model->vertex(model->face(i)[j]).x;
             face_pos[j].y = model->vertex(model->face(i)[j]).y;
             face_pos[j].z = model->vertex(model->face(i)[j]).z;
-            GetScreenPos(Vec2f(face_pos[j].x, face_pos[j].y), screen_pos[j]);
+            GetScreenPos(face_pos[j], screen_pos[j]);
         }
 
         Vec3f n = Vec3f(face_pos[2] - face_pos[0]) ^ (face_pos[1] - face_pos[0]);
         n.normalize();
         float intensity = n * LIGHT;
         if(intensity > 0)  // Back-face culling
-            DrawTriangle(screen_pos, image, TGAColor(intensity * 255, intensity * 255,intensity * 255, 255));
-
+            //DrawTriangle(screen_pos, image, TGAColor(intensity * 255, intensity * 255,intensity * 255, 255));
+            DrawTriangle_zBuffer(screen_pos, zBuffer, image, TGAColor(intensity * 255, intensity * 255,intensity * 255, 255));
     }
 
     image.flip_vertically();    // i want to have the origin at the left bottom corner of the image
